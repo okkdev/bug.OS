6557fd3a9633c7be141789dfde8c3bd25b4ea972
okkdev <dev@stehlik.me>
2021-11-17 17:30:00 +0100

    add cursor collision

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -1,7 +1,7 @@
 extends Node
 
+export var speed_multiplier = 1
 var current_pos = {"x": 0, "y": 0}
-var speed_multiplier = 1
 
 func _ready():
   Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -9,22 +9,22 @@ func _ready():
 
 func _input(event):
   if event is InputEventMouseMotion:
-    update_y(event.relative.y)
-    update_x(event.relative.x)
+    update_y(event.relative.y * speed_multiplier)
+    update_x(event.relative.x * speed_multiplier)
 
 func _process(delta):
-  $Sprite.position.x = current_pos.x
-  $Sprite.position.y = current_pos.y
+  self.position.x = current_pos.x
+  self.position.y = current_pos.y
 
 func update_y(y):
-  current_pos.y += y * speed_multiplier
+  current_pos.y += y
   if current_pos.y < 0:
     current_pos.y = 0
   elif current_pos.y > OS.window_size.y:
     current_pos.y = OS.window_size.y
     
 func update_x(x):
-  current_pos.x += x * speed_multiplier
+  current_pos.x += x
   if current_pos.x < 0:
     current_pos.x = 0
   elif current_pos.x > OS.window_size.x:
