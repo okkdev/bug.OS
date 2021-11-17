9a0932fbbb913a270f42abc5ee1091df086c09d0
okkdev <dev@stehlik.me>
2021-11-17 16:48:12 +0100

    basic mouse movement

---
+++ scripts/cursor.gd
@@ -0,0 +1,30 @@
+extends Node
+
+var current_pos = {"x": 0, "y": 0}
+
+func _ready():
+  Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+  current_pos = {"x": OS.window_size.x/2, "y": OS.window_size.y/2}
+
+func _input(event):
+  if event is InputEventMouseMotion:
+    update_y(event.relative.y)
+    update_x(event.relative.x)
+
+func _process(delta):
+  $Sprite.position.x = current_pos.x
+  $Sprite.position.y = current_pos.y
+
+func update_y(y):
+  current_pos.y += y
+  if current_pos.y < 0:
+    current_pos.y = 0
+  elif current_pos.y > OS.window_size.y:
+    current_pos.y = OS.window_size.y
+    
+func update_x(x):
+  current_pos.x += x
+  if current_pos.x < 0:
+    current_pos.x = 0
+  elif current_pos.x > OS.window_size.x:
+    current_pos.x = OS.window_size.x
