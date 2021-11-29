e3bb84590f98a75332479ac52214135daafcabf7
okkdev <dev@stehlik.me>
2021-11-29 21:56:07 +0100

    small changes

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -2,21 +2,20 @@ extends KinematicBody2D
 
 export var speed_multiplier = 1
 var current_pos = Vector2()
-var velocity = Vector2()
 
 func _ready():
   Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-  current_pos = {"x": OS.window_size.x/2, "y": OS.window_size.y/2}
+  current_pos = OS.window_size/2
 
 func _input(event):
   if event is InputEventMouseMotion:
     update_y(event.relative.y * speed_multiplier)
     update_x(event.relative.x * speed_multiplier)
-  
+
 func _physics_process(delta):
-    self.position.x = current_pos.x
-    self.position.y = current_pos.y
-    move_and_collide(velocity * delta)
+  self.position.x = current_pos.x
+  self.position.y = current_pos.y
+  move_and_collide(Vector2() * delta)
 
 func update_y(y):
   current_pos.y += y
