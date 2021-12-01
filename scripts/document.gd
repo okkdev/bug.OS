7ae815f5749122c8c5ba3fdf12bbe40fb33a27e5
okkdev <dev@stehlik.me>
2021-12-01 01:18:57 +0100

    add random labels

--- scripts/document.gd
+++ scripts/document.gd
@@ -5,6 +5,7 @@ var queue_deletion:bool = false
 
 func _ready():
   $Sprite.frame = randi() % $Sprite.get_sprite_frames().get_frame_count("default")
+  $Label.text = Global.predicates[randi() % Global.predicates.size()] + " " + Global.objects[randi() % Global.objects.size()]
 
 func _process(_delta):
   if not grabbed and queue_deletion:
--- scripts/global.gd
+++ scripts/global.gd
@@ -2,5 +2,22 @@ extends Node
 
 var cursor = null
 
+onready var objects_file = "res://assets/words/objects.txt"
+onready var predicates_file = "res://assets/words/predicates.txt"
+var objects: Array = []
+var predicates: Array = []
+
 func _ready():
-  pass
+    objects = load_file(objects_file)
+    predicates = load_file(predicates_file)
+
+func load_file(file) -> Array:
+    var f = File.new()
+    f.open(file, File.READ)
+
+    var res = []
+    while not f.eof_reached():
+        res.append(f.get_line())
+        
+    f.close()
+    return res
