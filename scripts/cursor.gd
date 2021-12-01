70ca80854b8ddea8c1cf9b83a300e96be1aab0f1
okkdev <dev@stehlik.me>
2021-12-01 10:40:52 +0100

    add window

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -4,7 +4,9 @@ export (float, -10000, 10000) var speed: float = 1000
 var velocity: Vector2 = Vector2.ZERO
 var grabbing: bool = false
 var hovering: bool = false
+var in_window: bool = false
 var document = null
+var window = null
 
 
 func _ready():
@@ -24,14 +26,21 @@ func _integrate_forces(s):
   
   
 func _process(delta):
-  if Input.is_mouse_button_pressed(1) and hovering:
+  if Input.is_mouse_button_pressed(1) and hovering and document:
     grabbing = true
     $Sprite.frame = 2
     document.grabbed = true
     document.position = self.position
-  elif hovering:
+  elif Input.is_mouse_button_pressed(1) and hovering and window:
+    window.queue_free()
+    window = null
+    in_window = false
+  elif hovering and document:
     $Sprite.frame = 1
     document.grabbed = false    
+    grabbing = false
+  elif hovering:
+    $Sprite.frame = 1
     grabbing = false
   else:
     $Sprite.frame = 0
@@ -39,14 +48,23 @@ func _process(delta):
 
 func _on_Area2D_area_entered(area):
   if not grabbing:
-    if area.is_in_group("document"):
+    if area.is_in_group("document") and not in_window:
       hovering = true
       document = area
-
+    if area.is_in_group("window"):
+      in_window = true
+    if area.is_in_group("close") and in_window:
+      hovering = true
+      window = area.get_parent()
 
 
 func _on_Area2D_area_exited(area):
   if not grabbing:
-    if area.is_in_group("document"):
+    if area.is_in_group("document") and not in_window:
       hovering = false
       document = null
+    if area.is_in_group("window"):
+      in_window = false
+    if area.is_in_group("close") and in_window:
+      hovering = false
+      window = null
--- scripts/main.gd
+++ scripts/main.gd
@@ -6,3 +6,8 @@ func _ready():
   var trash = Trash.instance()
   trash.position = $Documents/DocumentSpawnArea.rect_position + Vector2(randf() * $Documents/DocumentSpawnArea.rect_size.x, randf() * $Documents/DocumentSpawnArea.rect_size.y)
   add_child(trash)
+
+
+func _process(_delta):
+  if get_tree().get_nodes_in_group("document").size() == 0:
+    print("You win")
